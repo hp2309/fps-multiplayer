e54d70dae25337bb22d2f062decc60c0077c8a47
hp2309 <hetal.priyadarshi.23@gmail.com>
2021-06-27 21:36:10 +0530

    Connect to Master and Lobby

---
+++ FPS Multiplayer/Assets/Scripts/Launcher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+
+    void Start()
+    {
+        Debug.Log("connecting to Master");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("connected to Master");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Joined Lobby");
+    }
+
+    void Update()
+    {
+        
+    }
+}
