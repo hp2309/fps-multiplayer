d97f0216033c1ffe02a19c0a801273bcf400fb93
hp2309 <hetal.priyadarshi.23@gmail.com>
2021-06-27 22:53:41 +0530

    made Connecting and Title menu

---
+++ FPS Multiplayer/Assets/MenuManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager Instance;
+
+    [SerializeField] Menu[] menus;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void OpenMenu(string menuName)
+    {
+        foreach (Menu m in menus)
+        {
+            if (m.menuName == menuName)
+            {
+                OpenMenu(m);
+            }
+            else if(m.open)
+            {
+                CloseMenu(m);
+            }
+        }
+    } 
+    public void OpenMenu(Menu menu)
+    {
+        foreach (Menu m in menus)
+        {
+            if (m.open)
+            {
+                CloseMenu(m);
+            }
+        }
+        menu.Open();
+    } 
+    public void CloseMenu(Menu menu)
+    {
+        menu.Close();
+    } 
+}
