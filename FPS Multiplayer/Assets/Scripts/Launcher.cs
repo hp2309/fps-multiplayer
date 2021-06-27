44374ca1437d32c7abfff7de7bf07abae3e12dff
hp2309 <hetal.priyadarshi.23@gmail.com>
2021-06-27 23:51:29 +0530

    MenuManager System (#1)
    * Connect to Master and Lobby
    
    * made Title Menu Layout
    
    * made Connecting and Title menu
    
    * made Create room, room, error menu and UI buttons

---
+++ FPS Multiplayer/Assets/Scripts/Launcher.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    [SerializeField] TMP_InputField roomNameInputField;
+    [SerializeField] TMP_Text error_text;
+    [SerializeField] TMP_Text room_name_text;
+
+
+    void Start()
+    {
+        Debug.Log("connecting to Master");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("connected to Master");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        MenuManager.Instance.OpenMenu("title");
+        Debug.Log("Joined Lobby");
+    }
+
+    public void CreateRoom()
+    {
+        if (string.IsNullOrEmpty(roomNameInputField.text))
+        {
+            return;
+        }
+        PhotonNetwork.CreateRoom(roomNameInputField.text);
+        MenuManager.Instance.OpenMenu("loading");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("joined room");
+        room_name_text.text = PhotonNetwork.CurrentRoom.Name + "\'s room";
+        MenuManager.Instance.OpenMenu("room");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        error_text.text = "Room creation failed: " + message;
+        Debug.Log("Room Creation Failed: " + message);
+        MenuManager.Instance.OpenMenu("error");
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        MenuManager.Instance.OpenMenu("loading");
+    }
+
+    public override void OnLeftRoom()
+    {
+        MenuManager.Instance.OpenMenu("title");
+    }
+}
