9fb09743e85d0c3a1edaf9b83f195ab58176b119
hp2309 <hetal.priyadarshi.23@gmail.com>
2021-06-28 01:05:51 +0530

    Created Player Room join Room Exit
    Needs debugging in the room deletion and removal of player nickname from player list

--- FPS Multiplayer/Assets/Scripts/Launcher.cs
+++ FPS Multiplayer/Assets/Scripts/Launcher.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using TMPro;
+using Photon.Realtime;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+    public static Launcher Instance;
+
     [SerializeField] TMP_InputField roomNameInputField;
     [SerializeField] TMP_Text error_text;
     [SerializeField] TMP_Text room_name_text;
+    [SerializeField] Transform roomListContent;
+    [SerializeField] Transform playerListContent;
+    [SerializeField] GameObject roomListItemPrefab;
+    [SerializeField] GameObject playerListItemPrefab;
 
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     void Start()
     {
@@ -27,6 +38,7 @@ public class Launcher : MonoBehaviourPunCallbacks
     {
         MenuManager.Instance.OpenMenu("title");
         Debug.Log("Joined Lobby");
+        PhotonNetwork.NickName = "Player " + Random.Range(0, 100).ToString("000");
     }
 
     public void CreateRoom()
@@ -44,6 +56,13 @@ public class Launcher : MonoBehaviourPunCallbacks
         Debug.Log("joined room");
         room_name_text.text = PhotonNetwork.CurrentRoom.Name + "\'s room";
         MenuManager.Instance.OpenMenu("room");
+
+        Player[] players = PhotonNetwork.PlayerList;
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            Instantiate(playerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().Setup(players[i]);
+        }
     }
 
     public override void OnCreateRoomFailed(short returnCode, string message)
@@ -59,8 +78,32 @@ public class Launcher : MonoBehaviourPunCallbacks
         MenuManager.Instance.OpenMenu("loading");
     }
 
+    public void JoinRoom(RoomInfo info)
+    {
+        PhotonNetwork.JoinRoom(info.Name);
+        MenuManager.Instance.OpenMenu("loading");
+    }
+
     public override void OnLeftRoom()
     {
         MenuManager.Instance.OpenMenu("title");
     }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach (Transform tr in roomListContent)
+        {
+            Destroy(tr.gameObject);
+        }
+        foreach (RoomInfo ri in roomList)
+        {
+            Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItem>().Setup(ri);
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Instantiate(playerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().Setup(newPlayer);
+    }
+
 }
