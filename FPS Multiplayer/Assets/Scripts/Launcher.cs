0ee60b8b165940d023c6665edb6fb3f2951bbd5a
hp2309 <hetal.priyadarshi.23@gmail.com>
2021-06-27 23:47:29 +0530

    made Create room, room, error menu and UI buttons

--- FPS Multiplayer/Assets/Scripts/Launcher.cs
+++ FPS Multiplayer/Assets/Scripts/Launcher.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+    [SerializeField] TMP_InputField roomNameInputField;
+    [SerializeField] TMP_Text error_text;
+    [SerializeField] TMP_Text room_name_text;
+
 
     void Start()
     {
@@ -24,8 +29,38 @@ public class Launcher : MonoBehaviourPunCallbacks
         Debug.Log("Joined Lobby");
     }
 
-    void Update()
+    public void CreateRoom()
     {
-        
+        if (string.IsNullOrEmpty(roomNameInputField.text))
+        {
+            return;
+        }
+        PhotonNetwork.CreateRoom(roomNameInputField.text);
+        MenuManager.Instance.OpenMenu("loading");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("joined room");
+        room_name_text.text = PhotonNetwork.CurrentRoom.Name + "\'s room";
+        MenuManager.Instance.OpenMenu("room");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        error_text.text = "Room creation failed: " + message;
+        Debug.Log("Room Creation Failed: " + message);
+        MenuManager.Instance.OpenMenu("error");
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        MenuManager.Instance.OpenMenu("loading");
+    }
+
+    public override void OnLeftRoom()
+    {
+        MenuManager.Instance.OpenMenu("title");
     }
 }
