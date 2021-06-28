98e8e398778fb2789cbfc9a72bc20de8f65db145
hp2309 <hetal.priyadarshi.23@gmail.com>
2021-06-29 02:12:29 +0530

    added PlayerController, movement, look and instantiation

---
+++ FPS Multiplayer/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+
+public class PlayerManager : MonoBehaviour
+{
+    PhotonView PV;
+
+    private void Awake()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PV.IsMine)
+        {
+            CreateController();
+        }
+        
+    }
+
+    // Update is called once per frame
+    void CreateController()
+    {
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), new Vector3(15f,12f,39f), Quaternion.identity);
+    }
+}
